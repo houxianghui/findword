6ecc749c565267fed7d5107916567f38e1741869
houxianghui <houxianghui@126.com>
2018-10-13 00:42:15 +0800

    添加名单重复检查

--- src/com/blue/findword/FindWord.java
+++ src/com/blue/findword/FindWord.java
@@ -1,10 +1,13 @@
 package com.blue.findword;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -17,6 +20,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -25,40 +29,114 @@ import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 public class FindWord {
-	static Map<Long, String> map = new HashMap<>();
+	static Map<Long, List<Role>> map = new HashMap<>();
+	static Map<String, List<Role>> dupNameMap = new HashMap<>();
 	static HSSFCellStyle style;
-
+	static String[] white = new String[] { "老师", "顾问", "平行线" };
 	public static void main(String[] args) throws Exception{
 		File folder = new File("d:/group");
 		for(File t:folder.listFiles()) {
 			generate(t);
 		}
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("d:/重复名单.txt"), "utf-8"));
+		System.out.println("-----------------重复学号---------------------");
+		bw.write("-----------------重复学号---------------------");
+		bw.newLine();
+		for (Long l : map.keySet()) {
+			if (map.get(l).size() > 1) {
+				map.get(l).forEach(t -> {
+					System.out.print("[" + t + "]");
+					try {
+						bw.write("[" + t + "]");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+			} else {
+				continue;
+			}
+			System.out.println();
+			bw.newLine();
+		}
+		System.out.println("-----------------重复姓名---------------------");
+		bw.write("-----------------重复姓名---------------------");
+		bw.newLine();
+		for (String name : dupNameMap.keySet()) {
+			if (isInner(name)) {
+				continue;
+			}
+			if (dupNameMap.get(name).size() > 1) {
+				dupNameMap.get(name).forEach(t -> {
+					System.out.print("[" + t + "]");
+					try {
+						bw.write("[" + t + "]");
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				});
+			} else {
+				continue;
+			}
+			System.out.println();
+			bw.newLine();
+		}
+		bw.close();
+	}
+
+	private static boolean isInner(String name) {
+		for (String w : white) {
+			if (name.indexOf(w) != -1) {
+				return true;
+			}
+		}
+		return false;
 	}
 	
 	public static void generate(File f) throws Exception{
 		if (f.isDirectory()) {
 			return;
 		}
-//		System.out.println("processing "+f.getName());
-		
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(f),"utf8"));
 		String s  = null;
 		Pattern p = Pattern.compile("title=\"(\\d*.*?(\\d+))\"");
+		Pattern p2 = Pattern.compile("title=\"(.+?)\"");
 		List<Role> roles = new ArrayList<>();
 		while((s=br.readLine())!=null) {
+			boolean hasId = false;
 			Matcher m = p.matcher(s);
 			if(m.find()) {
+
 				Role r = new Role();
 				r.setName(m.group(1));
 				r.setId(Long.parseLong(m.group(2)));
-				roles.add(r);
-				if(map.containsKey(r.getId())) {
-					System.out.print("重复学号 ["+r.getName()+"]班级["+getClass(f)+"]");
-					System.out.println(" 和 "+map.get(r.getId()));
-				}else {
-					map.put(r.getId(), r.getName()+"班级["+getClass(f)+"]");
+				if (r.getId() > 5) {
+					hasId = true;
+					r.setClazz(getClass(f));
+					roles.add(r);
+
+					if (!map.containsKey(r.getId())) {
+						map.put(r.getId(), new ArrayList<>());
+					}
+					map.get(r.getId()).add(r);
 				}
-//				System.out.println(m.group(1)+"\t"+m.group(2));
+
+			}
+			if (hasId) {
+				continue;
+			}
+			Matcher m2 = p2.matcher(s);
+			if (m2.find()) {
+				Role r = new Role();
+				r.setName(m2.group(1));
+				if (StringUtils.isBlank(r.getName())) {
+					continue;
+				}
+				r.setClazz(getClass(f));
+				if (!dupNameMap.containsKey(r.getName())) {
+					dupNameMap.put(r.getName(), new ArrayList<>());
+				}
+				dupNameMap.get(r.getName()).add(r);
 			}
 		}
 		br.close();
@@ -96,6 +174,7 @@ public class FindWord {
 		wb.write(fos);
 		wb.close();
 		fos.close();
+
 	}
 
 	static SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
--- src/com/blue/findword/Role.java
+++ src/com/blue/findword/Role.java
@@ -2,18 +2,27 @@ package com.blue.findword;
 
 public class Role {
 	String name;
+	String clazz;
+
+	public String getClazz() {
+		return clazz;
+	}
+
+	public void setClazz(String clazz) {
+		this.clazz = clazz;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
-		this.name = name;
+		this.name = name.replaceAll("<img.*?/>", "");
 	}
 	
 	Long id;
 	
 	@Override
 	public String toString() {
-		return name+"\t"+id;
+		return clazz + "_" + name;
 	}
 	public Long getId() {
 		return id;
